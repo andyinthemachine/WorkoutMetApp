f559a6f0eea0ad1eb82f8922d7d8a111c8b93bab
Christian Pak <iamchristianpak@gmail.com>
2019-08-13 13:14:05 -0600

    got the single data, now working on rendering information per workout

--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -1,371 +1,293 @@
+
 import React from "react";
 import { RefreshControl, Platform, SectionList, StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
+import Swipeout from "react-native-swipeout";
+import moment from "moment/min/moment-with-locales";
 import { AntDesign, Ionicons } from "@expo/vector-icons";
-import { Stitch, RemoteMongoClient } from "mongodb-stitch-react-native-sdk";
-import Confetti from "react-native-confetti";
-import { TextInput } from "react-native-gesture-handler";
+import { Stitch, RemoteMongoClient, BSON } from "mongodb-stitch-react-native-sdk";
 
-export default class LinksScreen extends React.Component {
-    static navigationOptions = { header: null };
-
-    state = { 
-        calories: 0,
-        totalCal: 0,
-        met: 12,
-        min: 0,
-        weight: 52.2
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentUserId: undefined,
-            client: undefined,
-            workouts: undefined,
-            refreshing: false,
-        };
-        this._loadClient = this._loadClient.bind(this);
-    }
-
-    calculateCal = () => {
-        let simplifiedMet = 12/60
-        var caloriesBurned = (Math.floor(simplifiedMet * 60 ) * 52.2)
-        console.log(caloriesBurned)
-        this.setState({totalCal:caloriesBurned.toFixed(0)})
-    }
-
-    calculateTotal = () => {
-        
-    }
-
-    componentDidMount() {
-        console.log("cdmount");
-        this._loadClient();
-        this.calculateCal();
-        const { addListener } = this.props.navigation;
-        this.listeners = [addListener('didFocus', () => { this._loadClient(); })]
-    }
-
-    componentWillUnmount() {
-        this.listeners.forEach(sub => { sub.remove() })
-    }
-
-    _onRefresh = () => {
-        console.log("refresh");
-        this.setState({ refreshing: true });
-        const stitchAppClient = Stitch.defaultAppClient;
-        const mongoClient = stitchAppClient.getServiceClient(
-            RemoteMongoClient.factory,
-            "mongodb-atlas"
-        );
-        const db = mongoClient.db("workoutmanager");
-        const workouts = db.collection("workouts");
-        workouts
-            .find({ status: "new" }, { sort: { date: -1 } })
-            .asArray()
-            .then(docs => {
-                this.setState({ workouts: docs });
-                this.setState({ refreshing: false });
-            })
-            .catch(err => {
-                console.warn(err);
-            });
+export default class SettingsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUserId: undefined,
+      client: undefined,
+      workouts: undefined,
+      refreshing: false,
+      userName: "Joe"
     };
+    this._loadClient = this._loadClient.bind(this);
+  }
 
-    render() {
-        console.log("render");
+  componentDidMount() {
+    // this._loadClient();
+    const { addListener } = this.props.navigation;          
+    this.listeners = [addListener('didFocus', () => { this._loadClient();})]  
+  }
 
-        const sections =
-            this.state.workouts == undefined
-                ? [{ data: [{ title: "Loading..." }], title: "Loading..." }]
-                : this.state.workouts.length > 0
-                    ? [{ data: this.state.workouts, title: "Calories Burned" }]
-                    : [
-                        {
-                            data: [{ title: "No new workouts" }],
-                            title: "No new workouts"
-                        }
-                    ];
+  componentWillUnmount() {
+    this.listeners.forEach(
+      sub => { sub.remove() },
+    )
+  }
 
-        return (
-            <>
-            <SectionList
-                style={{ ...styles.container }}
-                renderItem={this._renderItem}
-                renderSectionHeader={this._renderSectionHeader}
-                stickySectionHeadersEnabled={true}
-                keyExtractor={(item, index) => index}
-                sections={sections}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={this.state.refreshing}
-                        onRefresh={this._onRefresh} />
-                }
-                renderSectionFooter={this._renderFooter}
+  
 
-            />
-            
-            </>
-        );
-    }
+  render() {
+    const { manifest } = Constants;
+    const sections =
+      this.state.workouts == undefined
+        ? [{ data: [{ title: "Loading..." }], title: "Loading..." }]
+        : this.state.workouts.length > 0
+          ? [{ data: this.state.workouts, title: "Edit workouts" }]
+          : [
+            {
+              data: [{ title: "No workouts" }],
+              title: "No workouts"
+            }
+          ];
 
-    _renderSectionHeader = ({ section }) => {
-        return <SectionHeader title={section.title} />;
-    };
-
-    _renderItem = ({ item }) => {
-        return (
-            <>
-                <SectionContent>
-                    <Confetti
-                        confettiCount={50}
-                        timeout={10}
-                        duration={2000}
-                        ref={node => (this._confettiView = node)} />
-                    <Text style={styles.sectionHeader}>{item.title != "No new workouts" ? item.description : ""}</Text>
-                    <View style={{ flex: 1, flexDirection: 'row' }}>
-                        <Text style={styles.sectionContentText}>
-                            Exercise
-                        </Text>
-                        <Text style={styles.operator}>
-                            x
-                    </Text>
-                        <TextInput
-                            style={styles.taskListTextTime}
-                            placeholder="Min"
-                            keyboardType='numeric'
-                            value={this.state.min}
-                        /> 
-                        <Text style={styles.operator}>
-                             =
-                    </Text>
-                        <Text style={styles.sectionContentText2}>
-                            {}12 cal
-                        </Text>
-                    </View>
-                </SectionContent>
-            </>
-        );
-    };
-    
-
-    _renderFooter = () => {
-        return (
-            <>
-                <SectionContent style={{paddingLeft: 0}}>
-                    <View style={{ flex: 1, flexDirection: 'row', paddingHorizontal: 50, backgroundColor: "darkgrey", color: "white"}}>
-                        <Text style={styles.sum}>Total Cals</Text>
-                        <Text style={styles.equal}>=</Text>
-                        <Text style={styles.totalCal} >≈  {this.state.totalCal} cal
-</Text>
-                    </View>
-                </SectionContent>
-            </>
-        );
-    };
-
-    _loadClient() {
-        console.log("load client");
-
-        const stitchAppClient = Stitch.defaultAppClient;
-        const mongoClient = stitchAppClient.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
-        const db = mongoClient.db("workoutmanager");
-        const workouts = db.collection("workouts");
-        workouts.find({ status: "new" }, { sort: { date: -1 } })
-            .asArray()
-            .then(docs => {
-                this.setState({ workouts: docs });
-            })
-            .catch(err => {
-                console.warn(err);
-            });
-    }
-
-    _onPressComplete(itemID) {
-        if (itemID) {
-            const stitchAppClient = Stitch.defaultAppClient;
-            const mongoClient = stitchAppClient.getServiceClient(
-                RemoteMongoClient.factory,
-                "mongodb-atlas"
-            );
-            const db = mongoClient.db("workoutmanager");
-            const workouts = db.collection("workouts");
-            workouts.updateOne(
-                { _id: itemID },
-                { $set: { status: "completed", completedDate: new Date() } },
-                { upsert: true })
-                .then(() => {
-                    workouts.find({ status: "new" }, { sort: { date: -1 } })
-                        .asArray()
-                        .then(docs => {
-                            this.setState({ workouts: docs });
-                            if (this._confettiView) {
-                                this._confettiView.startConfetti();
-                            }
-                        })
-                        .catch(err => {
-                            console.warn(err);
-                        });
-                })
-                .catch(err => {
-                    console.warn(err);
-                });
-        }
-    }
-
-    _onPressDelete(itemID) {
-        console.log("delete id = ", itemID);
-        const stitchAppClient = Stitch.defaultAppClient;
-        const mongoClient = stitchAppClient.getServiceClient(
-            RemoteMongoClient.factory,
-            "mongodb-atlas"
-        );
-        const db = mongoClient.db("workoutmanager");
-        const workouts = db.collection("worekouts");
-        workouts.deleteOne({ _id: itemID })
-            .then(() => {
-                console.log("deleteOne.then");
-                workouts.find({ status: "new" }, { sort: { date: -1 } })
-                    .asArray()
-                    .then(docs => {
-                        this.setState({ workouts: docs });
-                    })
-                    .catch(err => {
-                        console.warn(err);
-                    });
-            })
-            .catch(err => {
-                console.warn(err);
-            });
-    }
-}
-const SectionHeader = ({ title }) => {
     return (
-        <View style={styles.sectionHeaderContainer}>
-            <Text style={styles.sectionHeaderText}>{title}</Text>
-        </View>
+      <SectionList
+        style={{ ...styles.container }}
+        renderItem={this._renderItem}
+        renderSectionHeader={this._renderSectionHeader}
+        stickySectionHeadersEnabled={true}
+        keyExtractor={(item, index) => index}
+        sections={sections}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
+        }
+      />
     );
+  }
+
+  _renderSectionHeader = ({ section }) => {
+    return <SectionHeader title={section.title} />;
+  };
+
+  _renderItem = ({ item }) => {
+      console.log("Item:", item.description)
+      console.log("Item:", item.description)
+    return (
+      <SectionContent>
+        <Swipeout
+          autoClose={true}
+          backgroundColor="none"
+          right={[
+            {
+              component: (
+                <View
+                  style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column"
+                  }}
+                >
+                  <Ionicons
+                    name={Platform.OS == "ios" ? "ios-archive" : "md-archive"}
+                    size={30}
+                    style={{ textAlign: "center", color: "white" }}
+                  />
+                </View>
+              ),
+              backgroundColor: "#2e78b7",
+              onPress: () => this._onPressArchive(item._id)
+            }
+          ]}
+        >
+          <View style={styles.taskListTextTime}>
+            {item.title != "No workouts" &&
+              item.title != "Loading..." ? (
+                <Text style={styles.taskListTextTime}>
+                  created {moment(item.date).fromNow()}
+                </Text>
+              ) : item.title == "No workouts" ? (
+                <AntDesign
+                  name={Platform.OS == "ios" ? "smileo" : "smileo"}
+                  size={30}
+                  style={{
+                    textAlign: "center",
+                    color: "lightgray",
+                    marginTop: 25
+                  }}
+                />
+              ) : (
+                  <Text />
+                )}
+          </View>
+          <Text style={styles.sectionContentText}>
+            {item.title != "No workouts" ? item.description : ""}
+          </Text>
+          <Text style={styles.taskListTextTimeComplete}>
+            {item.title != "No workouts" && item.title != "Loading..."}
+          </Text>
+          <Text>
+              hello!
+          </Text>
+          <Text>
+              how aew toy
+          </Text>
+        </Swipeout>
+      </SectionContent>
+    );
+  };
+
+  _onRefresh = () => {
+    this.setState({ refreshing: true });
+    const stitchAppClient = Stitch.defaultAppClient;
+    const mongoClient = stitchAppClient.getServiceClient(
+      RemoteMongoClient.factory,
+      "mongodb-atlas"
+    );
+    const db = mongoClient.db("workoutmanager");
+    const workouts = db.collection("workouts");
+    workouts
+      .find(
+        { _id: new BSON.ObjectId("5d5304859b1aa282fea1f044") }
+      )
+      .asArray()
+      .then(docs => {
+        this.setState({ workouts: docs });
+        this.setState({ refreshing: false });
+      })
+      .catch(err => {
+        console.warn(err);
+      });
+  };
+
+  _loadClient() {
+    const stitchAppClient = Stitch.defaultAppClient;
+    const mongoClient = stitchAppClient.getServiceClient(
+      RemoteMongoClient.factory,
+      "mongodb-atlas"
+    );
+    const db = mongoClient.db("workoutmanager");
+    const workouts = db.collection("workouts");
+    workouts
+      .find(
+        { _id: new BSON.ObjectId("5d5304859b1aa282fea1f044") }
+      )
+      .asArray()
+      .then(docs => {
+        this.setState({ workouts: docs });
+      })
+      .catch(err => {
+        console.warn(err);
+      });
+  }
+
+  _onPressArchive(itemID) {
+    if (itemID) {
+
+      const stitchAppClient = Stitch.defaultAppClient;
+      const mongoClient = stitchAppClient.getServiceClient(
+        RemoteMongoClient.factory,
+        "mongodb-atlas"
+      );
+      const db = mongoClient.db("workoutmanager");
+      const workouts = db.collection("workouts");
+      workouts
+        .findOne(
+          { _id: new BSON.ObjectId("5d5304859b1aa282fea1f044") },
+          { $set: { status: "archived", archivedDate: new Date() } },
+          { upsert: true }
+        )
+        .then(() => {
+          workouts
+            .find({ status: "completed",
+            userName: this.state.userName }, { sort: { date: -1 } })
+            .asArray()
+            .then(docs => {
+              this.setState({ workouts: docs });
+            })
+            .catch(err => {
+              console.warn(err);
+            });
+        })
+        .catch(err => {
+          console.warn(err);
+        });
+    }
+  }
+}
+
+const SectionHeader = ({ title }) => {
+  return (
+    <View style={styles.sectionHeaderContainer}>
+      <Text style={styles.sectionHeaderText}>{title}</Text>
+    </View>
+  );
 };
 
 const SectionContent = props => {
-    return <View style={styles.sectionContentContainer}>{props.children}</View>;
+  return <View style={styles.sectionContentContainer}>{props.children}</View>;
 };
 
-LinksScreen.navigationOptions = {
-    headerTitle: (
-        <Ionicons
-            name={Platform.OS == "ios" ? "ios-create" : "md-create"}
-            size={23}
-            style={{
-                color: "#2e78b7",
-                flex: 1,
-                textAlign: "center"
-            }}
-            resizeMode="contain"
-        />
-    )
+SettingsScreen.navigationOptions = {
+  headerTitle: (
+    <Ionicons
+      name={
+        Platform.OS == "ios"
+          ? "ios-checkmark-circle-outline"
+          : "md-checkmark-circle-outline"
+      }
+      size={23}
+      style={{
+        color: "#2e78b7",
+        flex: 1,
+        textAlign: "center"
+      }}
+      resizeMode="contain"
+    />
+  )
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff"
-    },
-    sectionHeaderContainer: {
-        backgroundColor: "#fbfbfb",
-        paddingVertical: 8,
-        paddingHorizontal: 15,
-        borderWidth: StyleSheet.hairlineWidth,
-        borderColor: "#ededed",
-        alignItems: "center"
-    },
-    sectionHeaderText: {
-        fontSize: 14,
-        fontWeight: "bold"
-    },
-    sectionHeader: {
-        paddingTop: 2,
-        paddingLeft: 10,
-        paddingRight: 10,
-        paddingBottom: 2,
-        fontSize: 18,
-        fontWeight: 'bold',
-        backgroundColor: 'rgba(247,247,247,1.0)',
-        textAlign: 'left',
-      },
-    sectionContentContainer: {
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        borderBottomColor: "lightgray"
-    },
-    sectionContentText: {
-        color: "black",
-        flex: 2,
-        fontSize: 15,
-        paddingBottom: 10,
-        paddingHorizontal: 10,
-        paddingVertical: 15,
-        paddingRight: 5,
-        textAlign: "left",
-        flexDirection: 'row'
-    },
-    sectionContentText2: {
-        color: "black",
-        flex: 2,
-        fontSize: 15,
-        paddingBottom: 10,
-        paddingHorizontal: 10,
-        paddingVertical: 15,
-        textAlign: "right",
-        flexDirection: 'row',
-    },
-    taskListTextTime: {
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        paddingBottom: 10,
-        fontSize: 15,
-        textAlign: "center",
-        color: "blue",
-        flex: 1,
-        flexDirection: 'row',
-        fontWeight: "700"
-    },
-    operator: {
-        color: "black",
-        flex: 1,
-        fontSize: 15,
-        paddingBottom: 10,
-        paddingHorizontal: 10,
-        paddingVertical: 15,
-        textAlign: "center",
-        flexDirection: 'row',
-    },
-    sum: {
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        paddingBottom: 10,
-        fontSize: 15,
-        textAlign: "left",
-        color: "white",
-        fontWeight: "700",
-        flex: 1
-    },
-    equal: {
-        paddingVertical: 10,
-        paddingBottom: 10,
-        fontSize: 15,
-        textAlign: "center",
-        color: "white",
-        fontWeight: "700",
-        flex: 1
-    },
-    totalCal: {
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        paddingBottom: 10,
-        fontSize: 15,
-        textAlign: "right",
-        color: "white",
-        flex: 1,
-        fontWeight: "700"
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  sectionHeaderContainer: {
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#ededed",
+    alignItems: "center"
+  },
+  sectionHeaderText: {
+    fontSize: 14,
+    fontWeight: "bold"
+  },
+  sectionContentContainer: {
+    paddingHorizontal: 15,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "lightgray"
+  },
+  sectionContentText: {
+    color: "black",
+    fontSize: 15,
+    paddingBottom: 10,
+    paddingHorizontal: 10,
+    textAlign: "left"
+  },
+  taskListTextTime: {
+    paddingHorizontal: 15,
+    paddingVertical: 3,
+    textAlign: "center",
+    color: "lightgray"
+  },
+  taskListTextTimeComplete: {
+    paddingVertical: 3,
+    paddingHorizontal: 10,
+    textAlign: "left",
+    color: "green",
+    fontSize: 13
+  }
 });
